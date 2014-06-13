dae6052debafc99d93321a4af685cf5ab38cf856
itsuki <itsuki>
2014-06-13 13:12:00 +0800

    參考MVCSS、evenwu PPT調整CSS檔案架構，HTML slim化

--- config.rb
+++ config.rb
@@ -1,26 +1,4 @@
-# Require any additional compass plugins here.
 
-# Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "javascripts"
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
 
 css_dir = "css" # by Fire.app 
@@ -37,3 +15,6 @@ line_comments = true # by Fire.app
 sass_options = {:debug_info=>false} # by Fire.app 
 fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
 fireapp_livescript_options = {:bare=>false} # by Fire.app 
+
+require 'slim'
+Slim::Engine.set_default_options :pretty => true, :sort_attrs => true

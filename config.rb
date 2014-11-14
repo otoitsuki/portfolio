350d900a795e97249c5eeb38c11ba8a3b6c234ab
itsuki <otoitsuki@gmail.com>
2014-11-14 21:57:45 +0800

    add css map add susy2 修正title slim語法 追加各頁面對應page javascript merge helper 一些微調

--- config.rb
+++ config.rb
@@ -1,6 +1,6 @@
 # Require any additional compass plugins here.
 require 'slim'
-Slim::Engine.set_default_options :pretty => true, :disable_escape => true, :format => :html5
+Slim::Engine.set_default_options :pretty => true, :disable_escape => true, :format => :html5 
 
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
@@ -8,14 +8,6 @@ Slim::Engine.set_default_options :pretty => true, :disable_escape => true, :form
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
 preferred_syntax = :scss
-
-if ENV['RACK_ENV'] != 'production'
-  output_style = :nested # by Fire.app
-  line_comments = true # by Fire.app 
-else
-  output_style = :expand # by Fire.app
-  line_comments = false # by Fire.app 
-end
 
 css_dir = "styles" # by Fire.app 
 sass_dir = "sass" # by Fire.app 
@@ -25,7 +17,10 @@ fireapp_coffeescripts_dir = "coffeescripts" # by Fire.app
 fireapp_livescripts_dir = "livescripts" # by Fire.app 
 fireapp_minifyjs_on_build = true # by Fire.app 
 fireapp_always_report_on_build = true # by Fire.app 
+output_style = :compressed # by Fire.app 
 relative_assets = false # by Fire.app 
+line_comments = false # by Fire.app 
 sass_options = {:debug_info=>false} # by Fire.app 
+sourcemap = true # by Fire.app 
 fireapp_coffeescript_options = {:bare=>false} # by Fire.app 
 fireapp_livescript_options = {:bare=>false} # by Fire.app 
